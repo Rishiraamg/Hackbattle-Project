7cd90e4886a93eaa45dd593478dcaa1f9f420fdb
Varun-2604 <mvarun2604@gmail.com>
2023-09-20 02:07:16 +0530

    login page flask implementation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import MySQLdb
 from flask import Flask, request, render_template, redirect, url_for
+
+dcobj=MySQLdb.connect('localhost','root','admin')
+cj=dcobj.cursor()
 
 app = Flask(__name__)
 
@@ -17,15 +21,33 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
+        
+        cj.execute("use VITRAVEL")
+        cj.execute("select * from Login where Username=%s and Password=%s",(username,password))
+        table=cj.fetchone()
+        
         # Check if the entered credentials match the stored credentials
-        if username in user_credentials and user_credentials[username] == password:
+        #if username in user_credentials and user_credentials[username] == password:
+        if table:
             return "Login Successful!"
+            #redirect here
         else:
             error = "Invalid credentials. Please try again."
             return render_template('login.html', error=error)
 
     return render_template('login.html', error=None)
 
+def createdb():
+    cj.execute('create database VITRAVEL')
+    cj.execute('use VITRAVEL')
+    cj.execute('create table Login (Username varchar(100), Password varchar(50))')
+    dcobj.commit()
+    
+
+
+
+
 if __name__ == '__main__':
+    #createdb()
     app.run(debug=True)
+    
